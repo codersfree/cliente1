539a079a5481027acec79417da04e3f9bcb5c03d
victor arana <victor@codersfree.com>
2021-06-28 15:23:03 -0500

    mandar acces token

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,22 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('posts', function () {
+
+    $response = Http::withHeaders([
+
+        'Accept' => 'application/json',
+        'Authorization' => 'Bearer ' . auth()->user()->accessToken->access_token
+        
+    ])->post('http://api.codersfree.test/v1/posts', [
+        'name' => 'Esto es una prueba de post',
+        'slug' => 'esto-es-una-prueba-de-post',
+        'extract' => 'sdsafsdfsd',
+        'body' => 'asdasdasdasdasd',
+        'category_id' => 1
+    ]);
+
+    return $response->json();
+
+})->middleware('auth');
